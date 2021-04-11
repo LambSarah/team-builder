d36516da1621bd155a7c647b3ddaf08989dda4a1
Sarah Guidry <sarahbethguidry@gmail.com>
2021-04-11 13:39:13 -0500

    Added controlled inputs

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -4,65 +4,60 @@ import Form from './Components/Form';
 import './App.css';
 
 const App = () => {
-  const [memberList, setMemberList] = useState([]);
+	const [memberList, setMemberList] = useState([]);
 
-  const [member, setMember] = useState({
-    name: '',
-    email: '',
-    role: '',
-  });
+	const [member, setMember] = useState({
+		name: '',
+		email: '',
+		role: '',
+	});
 
-  const handleChange = event => {
-    setMember({
-      ...member,
-      [event.target.name]: event.target.value,
-    });
-  };
+	const handleChange = event => {
+		setMember({
+			...member,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-  const members = [];
+	const members = [];
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    members.push(member);
-    setMemberList(members);
-  };
+	const handleSubmit = event => {
+		event.preventDefault();
+		members.push(member);
+		setMemberList(members);
+	};
 
-  return (
-    <Router>
-      <div className="App">
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/form">Add Member</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="fellowMembers">
-          {memberList.map(person => {
-            return (
-              <div className="memberInfo">
-                <li> Name : {member.name}</li> <li> Email: {member.email}</li>
-                <li> Role : {member.role}</li>
-              </div>
-            );
-          })}
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/form" component={Form} />
-          </Switch>
-          <Form
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-            member={member}
-            memberList={memberList}
-          />
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<nav className="navbar">
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/form">Add Member</Link>
+						</li>
+					</ul>
+				</nav>
+				<div className="fellowMembers">
+					{memberList.map(person => {
+						return (
+							<div className="memberInfo">
+								<li> Name : {member.name}</li> <li> Email: {member.email}</li>
+								<li> Role : {member.role}</li>
+							</div>
+						);
+					})}
+					<Switch>
+						<Route exact path="/" component={App} />
+						<Route path="/form" component={Form} />
+					</Switch>
+					<Form handleChange={handleChange} handleSubmit={handleSubmit} />
+				</div>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
 );
