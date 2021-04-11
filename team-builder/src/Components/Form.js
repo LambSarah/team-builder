60b7134c704c3336370b5a4d8cf1882d797ab8b0
Sarah Guidry <sarahbethguidry@gmail.com>
2021-04-11 13:14:39 -0500

    Updated Form.js

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Form = props => {
+  const { handleChange, handleSubmit } = props;
+
+  return (
+    <div className="form-container">
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Role:
+          <input
+            type="text"
+            name="role"
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default Form;
